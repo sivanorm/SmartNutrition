d1cb63e1809da3970194f6bcc8f4276c6d3c35a4
sivanorm <sivanormsoft@gmail..com>
2024-02-02 16:00:48 +0530

    Commit by script

--- root/hosting/server/main.server.mjs
+++ root/hosting/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-OMCVB234.mjs";import"./chunk-DWFBVJFV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-5DY5VXAN.mjs";import"./chunk-DWFBVJFV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
